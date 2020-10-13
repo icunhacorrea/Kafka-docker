4a2c25139863113142f658a8e19d97500d326d42
icunhacorrea <iago.cunhac@gmail.com>
2020-10-13 20:59:53 +0000

    Testando com pumba.

--- start-services.sh
+++ start-services.sh
@@ -2,7 +2,7 @@
 
 docker-compose down
 docker-compose up -d
-sleep 3
-docker container exec kafka-docker_kafka1_1 bash create-topic.sh 3 3 test-topic
+sleep 5
+docker container exec kafka-docker_kafka3_1 bash create-topic.sh 3 3 test-topic
 docker container logs kafka-docker_monitor1_1 -f
 
