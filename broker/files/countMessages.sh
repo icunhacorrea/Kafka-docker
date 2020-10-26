e7d4290c82b23208f3b3d19110536d53cfc288a6
icunhacorrea <iago.cunhac@gmail.com>
2020-10-26 11:59:57 -0300

    Enviando docker compose com falhas pro lab.

--- broker/files/countMessages.sh
+++ broker/files/countMessages.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-$KAFKA_HOME/bin/kafka-run-class.sh kafka.tools.GetOffsetShell --broker-list kafka3:9092 \
+$KAFKA_HOME/bin/kafka-run-class.sh kafka.tools.GetOffsetShell --broker-list 172.21.0.5:9092 \
 	--topic test-topic --time -1 --offsets 1 | awk -F  ":" '{sum += $3} END {print sum}'
--- broker/files/create-topic.sh
+++ broker/files/create-topic.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-$KAFKA_HOME/bin/kafka-topics.sh --create --bootstrap-server kafka1:9092,kafka2:9092,kafka3:9092 \
+$KAFKA_HOME/bin/kafka-topics.sh --create --bootstrap-server 172.21.0.5:9092,172.21.0.6:9092,172.21.0.7:9092 \
 								--replication-factor $1 --partitions $2 --topic $3 \
 								--config min.insync.replicas=$1
 
