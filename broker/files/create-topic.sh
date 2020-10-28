3d415fa04970e862918045b25f25bcff406e4a37
icunhacorrea <iago.cunhac@gmail.com>
2020-10-28 17:28:23 -0300

    ...

--- broker/files/create-topic.sh
+++ broker/files/create-topic.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-$KAFKA_HOME/bin/kafka-topics.sh --create --bootstrap-server kafka1:9092,kafka2:9092,kafka3:9092 \
+$KAFKA_HOME/bin/kafka-topics.sh --create --bootstrap-server 172.21.0.5:9092,172.21.0.6:9092,172.21.0.7:9092 \
 								--replication-factor $1 --partitions $2 --topic $3 \
 								--config min.insync.replicas=$1
 
-kafkacat -L -b kafka1:9092,kafka2:9092,kafka3:9092 -t $3
+kafkacat -L -b 172.21.0.5:9092,172.21.0.6:9092,172.21.0.7:9092 -t $3
