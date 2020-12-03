101254e86e8c96a0521594b5e3877de958afe0f6
icunhacorrea <iago.cunhac@gmail>
2020-12-03 18:16:10 -0300

    ...

--- lossPackets.sh
+++ lossPackets.sh
@@ -5,22 +5,16 @@ for percent in 0 2 4 6 8 10; do
 		for i in {0..19}; do
 			echo $i $ack $percent
 
-			docker-compose down
-
-			docker system prune -f
-
 			docker-compose up -d
 			sleep 5
 			docker container exec kafka-docker_kafka3_1 bash create-topic.sh 3 3 test-topic
-
 			# Inserção da falha
 			pumba netem -d 3600h loss-state --p13 0 --p14 $percent  $( dock ps | grep -e kafka\[0-9] -e kafka-producer | awk '{ print $NF }' ) &
-
 			docker container exec kafka-docker_producer_1 bash execute.sh test-topic $ack 500000 500 > ./logs/log-$percent-$ack-$i.log
-
-
 			# Remoção da falha.
 			kill -9 $( pgrep pumba )
+			docker-compose down -v
+			docker system prune -f
 
 		done
 	done
