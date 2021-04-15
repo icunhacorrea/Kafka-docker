14320128b241211970cfac6d4beabb59e3324cce
icunhacorrea <iago.cunhac@gmail.com>
2021-04-15 21:23:06 +0000

    100ms final results.

--- insertDelay.sh
+++ insertDelay.sh
@@ -6,7 +6,7 @@ while true; do
 	count=$( docker container exec kafkadocker_kafka1_1 bash countMessages.sh )
 	echo "Count: $count; Delay: $1"
 	if [ $count -gt $LIMIT_MESAGES ]; then
-		echo "Introduzindo falha..."
+		echo "Introduzindo falha... delay: $1; ack: $2; exec: $3" >> ./logs/log-monitor-$1-$2-$3.out
 		pumba netem -d 60s delay -t $1 $( docker ps | grep kafka-producer | awk '{ print $NF }' )
 		break
 	fi
