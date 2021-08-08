4432d706e83c086d7a1290e380ece9546e7248b5
icorrea <iago.cunhac@gmail.com>
2021-08-08 10:12:47 -0300

    Adjusting graphs size

--- plots/lossPlot.py
+++ plots/lossPlot.py
@@ -18,22 +18,23 @@ df['ack'] = df['ack'].replace({'1': 'Leader Confirm.'})
 
 print(df) 
 
-sns.set(style = 'ticks')
+sns.set(style = 'ticks', font_scale=1.3)
 
 hue_order = ['Fire and Forget', 'Reliable Ack', 'Leader Confirm.', 'Replicas Confirm.']
 
-g = sns.barplot(x='tempo', y='chance', hue='ack', orient='h', data=df,
+g = sns.barplot(x='vazao', y='chance', hue='ack', orient='h', data=df,
 				errcolor='black', capsize=0.04, errwidth=2, saturation=8, ci="sd",
 				edgecolor=".08", linewidth=0.04, hue_order=hue_order)
 g.legend_.set_title(None)
-g.xaxis.set_major_locator(ticker.MultipleLocator(5000))
+g.xaxis.set_major_locator(ticker.MultipleLocator(1000))
+#g.xaxis.set_major_locator(ticker.MultipleLocator(5000))
 g.xaxis.set_major_formatter(ticker.ScalarFormatter())
 
 plt.gcf().set_size_inches(15.7, 5.27)
-plt.ylabel('Chance de perder pacote a cada envio (%)')
-#plt.xlabel('Vazão (mensagens/seg)')
-plt.xlabel('Tempo de execução (seg)')
+plt.ylabel("% de perda de pacote")
+plt.xlabel('Vazão (msg/seg)')
+#plt.xlabel('Tempo de execução (seg)')
 
-#plt.savefig('./pdfs/loss-vazao-FF.pdf', bbox_inches='tight', dpi=600)
-plt.savefig('./pdfs/loss-tempo-FF.pdf', bbox_inches='tight', dpi=600)
+plt.savefig('./pdfs/loss-vazao-FF.pdf', bbox_inches='tight', dpi=600)
+#plt.savefig('./pdfs/loss-tempo-FF.pdf', bbox_inches='tight', dpi=600)
 plt.clf()
